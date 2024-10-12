98bdec8451c9e9727bbb8f6b70a2a3f50732a3e8
Andrey Basov <andronbas@gmail.com>
2024-10-12 23:40:18 +0300

    fix gendiff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -8,20 +8,33 @@ def generate_diff(file1, file2):
     data2 = read_file(file2)
     if data1 is None or data2 is None:
         return "Error in reading files."
+    return build_diff(data1, data2)
 
-    diff_result = []
 
-    all_keys = sorted(set(data1.keys()).union(set(data2.keys())))
+def handle_reading_error(data1, data2):
+    if data1 is None or data2 is None:
+        return "Error in reading files."
+    elif data1 is None:
+        return "Error in reading file first file."
+    else:
+        return "Error in reading file second file."
+
+
+def build_diff(data1, data2):
+    """Создает различия между двумя словарями."""
+    diff = []
+    all_keys = sorted(data1.keys() | data2.keys())
 
     for key in all_keys:
-        if key not in data2:
-            diff_result.append(f"  - {key}: {data1[key]}")
-        elif key not in data1:
-            diff_result.append(f"  + {key}: {data2[key]}")
-        elif data1[key] != data2[key]:
-            diff_result.append(f"  - {key}: {data1[key]}")
-            diff_result.append(f"  + {key}: {data2[key]}")
+        if key in data1 and key in data2:
+            if data1[key] == data2[key]:
+                diff.append(f"    {key}: {data1[key]}")
+            else:
+                diff.append(f"  - {key}: {data1[key]}")
+                diff.append(f"  + {key}: {data2[key]}")
+        elif key in data1:
+            diff.append(f"  - {key}: {data1[key]}")
         else:
-            diff_result.append(f"    {key}: {data1[key]}")
+            diff.append(f"  + {key}: {data2[key]}")
 
-    return "{\n" + "\n".join(diff_result) + "\n}"
+    return "{\n" + "\n".join(diff) + "\n}"
